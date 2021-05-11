26a176f726f1b2e96bc0ea5841abd05e85cb61e2
Dodz <37667105+dodzcf@users.noreply.github.com>
2021-05-11 10:33:21 -0700

    Add files via upload

---
+++ nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './nav.css';
+
+function Nav()
+{
+    return (
+        <nav class="navbar">
+        <span class="navbar-brand mb-0 h1"><strong>Netflix Series</strong></span>
+        </nav>
+    );
+}
+
+export default Nav;
